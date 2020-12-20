1e39ed2edffb662398716a340a308f9cd793c7a9
mp5maker <khan.photon@gmail.com>
2020-12-20 17:19:33 +0600

    chore: update git ignore

--- expo-app/App.tsx
+++ expo-app/App.tsx
@@ -1,8 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// import { adder } from '@web/utilities/adder'
 
 export default function App() {
+
+  // console.log(adder(1, 2))
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
---
+++ web-app/src/utilities/adder.tsx
@@ -0,0 +1,1 @@
+export const adder = (value1: number, value2: number): number => value1 + value2
